315524a6692f72c5e1b39619fd86b20aff5c8ed7
Justin Epstein <epsteinapps@gmail.com>
2022-04-17 21:17:19 -0400

    Initial Commit

---
+++ Stopwatch/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Stopwatch
+//
+//  Created by Justin Epstein on 4/17/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Stopwatch/StopwatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  StopwatchApp.swift
+//  Stopwatch
+//
+//  Created by Justin Epstein on 4/17/22.
+//
+
+import SwiftUI
+
+@main
+struct StopwatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
